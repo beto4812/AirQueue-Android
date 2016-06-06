11ee5a9ab351cac53c124db5a4d3b5bcab5f662d
Alberto Vazquez <beto4812@hotmail.com>
2016-06-06 12:12:42 +0100

    Added AWS API

--- app/src/main/java/com/beto4812/airqueue/AirQueue.java
+++ app/src/main/java/com/beto4812/airqueue/AirQueue.java
@@ -4,6 +4,10 @@ import android.app.Application;
 
 import com.firebase.client.Firebase;
 import com.firebase.client.Logger;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+
 
 public class AirQueue extends Application {
     @Override
@@ -11,5 +15,11 @@ public class AirQueue extends Application {
         super.onCreate();
         Firebase.setAndroidContext(this);
         Firebase.getDefaultConfig().setLogLevel(Logger.Level.DEBUG);
+
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),    /* get the context for the application */
+                "COGNITO_IDENTITY_POOL",    /* Identity Pool ID */
+                Regions.US_EAST_1           /* Region for your identity pool--US_EAST_1 or EU_WEST_1*/
+        );
     }
 }
